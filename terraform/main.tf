69663ad4503eb9da7c8ebb2b4158558b6d512a67
Tim Fletcher <tfletcher@thoughtworks.com>
2020-10-08 17:54:36 +0200

    Create a service and ingress for the cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,8 +32,44 @@ resource "kubernetes_deployment" "nginx" {
         container {
           image = "nginx"
           name  = "hello-nginx"
+          port {
+            container_port = 80
+          }
         }
       }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+    namespace = kubernetes_namespace.cluster.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.metadata[0].labels.app
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 8080
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_ingress" "ingress" {
+  metadata {
+    name = "example-ingress"
+    namespace = kubernetes_namespace.cluster.metadata[0].name
+  }
+
+  spec {
+    backend {
+      service_name = kubernetes_service.nginx.metadata[0].name
+      service_port = kubernetes_service.nginx.spec[0].port[0].port
     }
   }
 }
